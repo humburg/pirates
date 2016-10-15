f25a9708c8af777502fe2bed9da698fec91e3f92
jtj02 <johnj@ieee.org>
2016-10-15 13:52:21 +1100

    Fixed bug where each line in full was printed out

--- label-reading/list-labels.py
+++ label-reading/list-labels.py
@@ -4,7 +4,6 @@ c = 0
 for line in f:
     if (c % 4) == 1:
         line = line.rstrip("\n")
-        print line
         nameid = line[0:8] + line[-8:]
         print nameid, line[8:12], line[-12:-8]
     c = c + 1
